ff007018ce6bd724eeccb4879f91a9ed7fca7930
githrahat <86165666+githrahat@users.noreply.github.com>
2022-12-30 16:03:36 +0600

    Add files via upload

---
+++ 03_final-const.dart
@@ -0,0 +1,13 @@
+void main(){
+
+
+  // final and const are same, they stay as fixed which assign value cant be changed or override. 
+  final world ='Earth';
+   print(world);
+
+  const city ='Sylhet';
+    //city ='Dhaka';
+  print(city);
+
+  
+}
