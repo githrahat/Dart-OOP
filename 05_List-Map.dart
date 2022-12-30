7a630e7b23fffa30c89c1c1000c8660d7e315075
githrahat <86165666+githrahat@users.noreply.github.com>
2022-12-30 16:04:29 +0600

    Add files via upload

---
+++ 05_List-Map.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  // List
+  List<String> subjects = ['science', 'math', 'english', 'bangla'];
+  print(subjects);
+
+  // prints the first 
+  var subfirst = subjects.first;
+  print(subjects.first);
+  print(subjects.last);
+  print(subjects.length);
+  print(subjects.reversed);
+  print(subjects[3]);
+
+ /*
+  var firstItem = list.first;
+  print(firstItem);
+  var lastItem = list.last;
+  print(lastItem);
+  int size = list.length;
+  print(size);
+  var reverse = list.reversed;
+  print(reverse);
+*/
+}
