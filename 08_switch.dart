a4f4abb8e6e7bdc7c86154b254f5c6cd5510e223
githrahat <86165666+githrahat@users.noreply.github.com>
2022-12-30 16:05:34 +0600

    Add files via upload

---
+++ 08_switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  var day = 'b';               // it can work for int too same as string, var num = 10;
+  switch(day){                // switch(num){
+    case 'a':                //  case 1:
+      print('Sunday');      //     print('good');
+      break;               //      break;
+    case 'b':             //     case 2:
+      print('monday');   //        print('very good');
+      break;
+    case 'c':
+      print('tuesday');
+      break;
+    
+    default:
+      print('out of option');
+
+
+  }
+}
